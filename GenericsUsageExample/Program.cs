9584d5c4c62d07339473519b13dcf070f47e2e32
Michał Kurpiński <michal.kurpinski96@gmail.com>
2017-09-10 11:21:13 +0200

    Add project files.

---
+++ GenericsUsageExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsUsageExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
